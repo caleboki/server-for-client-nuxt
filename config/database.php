37f410e7f0d5005f45312d304c75dd5273e3f1b9
Caleb Oki <caleboki@gmail.com>
2020-03-01 15:16:49 +0100

    changed db credentials

--- config/database.php
+++ config/database.php
@@ -46,6 +46,26 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'dbfree.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravuetube'),
+            'username' => env('DB_USERNAME', 'laravuetube_root'),
+            'password' => env('DB_PASSWORD', '629a4876'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
